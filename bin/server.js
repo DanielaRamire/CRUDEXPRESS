fb2c096a62d1debdf17dbd593a5c90a252cae865
Chris <cgolayac@correo.udistrital.edu.co>
2023-02-21 13:18:18 -0500

    creating and setting environment variables

--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,25 @@
-// const http = require('http');
-// const app = require('../app');
+require('dotenv').config()
 
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
 
-// const server = http.createServer(app);
+const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+client.connect((err) => {
+    if (err) {
+        console.log('Error connecting to MongoDB:', err);
+    } else {
+        console.log('Connected to MongoDB');
+        const db = client.db('<database>');
+        const collection = db.collection('<collection>');
 
-// server.listen(3000);
+        // Aquí puedes realizar operaciones en la colección
 
-// server.on('listening', () => {
-//     console.log('servidor 3000');
-// });
+        client.close();
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
