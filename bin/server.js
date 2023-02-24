9854f40dda1949535a53182024047819d82db878
Chris <cgolayac@correo.udistrital.edu.co>
2023-02-24 15:44:57 -0500

    Modify code for postman documentation

--- bin/server.js
+++ bin/server.js
@@ -1,24 +1,38 @@
 require('dotenv').config()
 
+const mongoose = require('mongoose');
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const port = process.env.Port || 9000;
 
-client.connect((err) => {
-    if (err) {
-        console.log('Error connecting to MongoDB:', err);
-    } else {
-        console.log('Connected to MongoDB');
-        const db = client.db('<database>');
-        const collection = db.collection('<collection>');
+// client.connect((err) => {
+//     if (err) {
+//         console.log('Error connecting to MongoDB:', err);
+//     } else {
+//         console.log('Connected to MongoDB');
+//         const db = client.db('<database>');
+//         const collection = db.collection('<collection>');
 
-        // Aquí puedes realizar operaciones en la colección
-        client.close();
-    }
+//         Aquí puedes realizar operaciones en la colección
+//         client.close();
+//     }
+// });
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() =>  console.log("Connected to MongoDB Atlas"))
+    .catch((error) => console.error(error))
+
+app.listen(port, () => console.log('server listening on port', port));
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+app.use(express.json());
+app.use(require('../routes'));
+
+module.exports = app;
